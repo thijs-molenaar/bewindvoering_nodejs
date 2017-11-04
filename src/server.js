70429ef43a4e868c48c60b1df55f0854e6200874
Thijs Molenaar <thijsmolenaar+gh@gmail.com>
2017-11-04 16:48:08 +0100

    Moves bundle.js to js folder. Adds Express support.

--- src/server.js
+++ src/server.js
@@ -1,13 +1,38 @@
-var http = require('http');
-var formidable = require('formidable');
+var Express = require('express');
+var multer = require('multer');
+var bodyParser = require('body-parser');
+var path = require('path');
+var app = Express();
+app.use(bodyParser.json());
 
-http.createServer( (req, res) => {
+// serve js as static files
+app.use("/js", Express.static(path.resolve(__dirname + "/../dist/js")));
 
-  if ( req.url === "/test" ){
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write("test page reached");
-    res.end();
-  }
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write("What's up, doc?");
-} ).listen(8080);
+var Storage = multer.diskStorage({
+    destination: function(req, file, callback) {
+        callback(null, "/../dist/uploads");
+    },
+    filename: function(req, file, callback) {
+        callback(null, file.fieldname + "_" + Date.now() + "_" + file.originalname);
+    }
+});
+
+var upload = multer({
+     storage: Storage
+ }).array("imgUploader", 3); //Field name and max count
+
+app.get("/", function(req, res) {
+     res.sendFile( path.resolve(__dirname + "/../dist/index.html"));
+     //res.redirect('index.html');
+ });
+
+ app.post("/api/Upload", function(req, res) {
+     upload(req, res, function(err) {
+         if (err) {
+             return res.end("Something went wrong!");
+         }
+         return res.end("File uploaded sucessfully!.");
+     });
+ });
+
+ app.listen(8080, () => console.log('Listening on port 8080'))
